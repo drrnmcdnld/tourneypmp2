243bd89410aea4628ef7fde2fe6e613944dc30b7
Rob Myers <robert_myers_@hotmail.com>
2014-10-18 17:52:53 -0300

    Cleanup of login

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -31,7 +31,13 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
+            if (Yii::app()->user->isGuest) {
 		$this->render('index');
+            }
+            else {
+                $this->render('/tournament/index', array('dataProvider'=>array()));
+            }
+                
 	}
 
 	/**
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,4 +6,8 @@ $this->pageTitle="TourneyPimp, pimp my tournament!";
 
 <h2>Pimp my Tournament!</h2>
 
+<?php if (Yii::app()->user->isGuest) { ?>
 <p>Welcome to tourneypimp, your online stop for sports tournaments!  Please log in or create an account to add or manage a tournament, or select a tournament from the listing below.</p>
+<?php } else { ?>
+<p> Logged in!</p>
+<?php } ?>
