c2f76e797b8e174c89e27ab060afb4538f0d7df5
Rob Myers <robert_myers_@hotmail.com>
2014-10-19 17:35:58 -0300

    Linking dashboard with module

---
+++ protected/views/layouts/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<!-- This site was created in Webflow. http://www.webflow.com-->
+<!-- Last Published: Sun Oct 19 2014 19:45:12 GMT+0000 (UTC) -->
+<html data-wf-site="5442d721e76d050f4de8a095" data-wf-page="5442d721e76d050f4de8a097">
+<head>
+  <meta charset="utf-8">
+  <title>TourneyPMP</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="generator" content="Webflow">
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/webflow.css">
+  <link rel="stylesheet" type="text/css" href="css/tourneypmp.webflow.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+  <script>
+    WebFont.load({
+      google: {
+        families: ["Montserrat:400,700","Bitter:400,700,400italic"]
+      }
+    });
+  </script>
+  <script type="text/javascript" src="js/modernizr.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="images/webclip-slate.png">
+  <link rel="apple-touch-icon" href="images/webclip-slate.png">
+</head>
+<body>
+  <header class="navbar">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-4 w-clearfix">
+          <img class="logo" src="images/TourneyPMPlogo.png" width="200" alt="5443b4680fdff0c53ed03f9e_TourneyPMP%20Logo.png">
+        </div>
+        <div class="w-col w-col-8 nav-column"><a class="nav-link" href="/User/create">Register</a><a class="nav-link" href="/site/login">Login</a><a class="nav-link" href="/Tournament/viewTournaments">current tournaments</a>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="section hero">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6">
+          <img class="hero-iphone" src="images/TourneyPMPiPhone.png" alt="iPhone" width="1450">
+        </div>
+        <div class="w-col w-col-6 call-to-action">
+          <h1 class="hero-header">Build a tournament</h1>
+          <p class="hero-subtitle">TourneyPMP can track your league's standings, get directions to the game, and give you real time score &amp;&nbsp;updates.</p><a class="button" href="/User/create">Register Here</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-hidden-small w-hidden-tiny section grey"></div>
+  <div class="w-clearfix section" id="features">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6">
+          <h2>Setup multiple venues</h2>
+          <p>Have you ever struggled with setting up a tournament that had multiple locations?&nbsp;&nbsp;TourneyPMP has your back, setup multiple arenas, fields, etc..and focus on your promoting your tournament.</p>
+        </div>
+        <div class="w-col w-col-6 center">
+          <img src="images/artificial-baseball-field.jpg" alt="5443fb266bd084c63e8a91b3_artificial-baseball-field.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section grey">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6 center">
+          <img src="images/FB-8218-2.jpg" width="2700" alt="5443fb606bd084c63e8a91b8_FB-8218-2.jpg">
+        </div>
+        <div class="w-col w-col-6">
+          <h2>Get real-time scores</h2>
+          <p>Need to know who you are playing or what the standings are?&nbsp;&nbsp;Get real-time scores and updated standings to make informed decisions about where to go next.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6 w-clearfix">
+          <h2>Find your next game</h2>
+          <p><strong>Coming soon to a field or arena near you!<br xmlns="http://www.w3.org/1999/xhtml"></strong>Use our fully integrated map program to find the exact location of your game!</p>
+          <div class="w-widget w-widget-twitter social-widget">
+            <iframe src="https://platform.twitter.com/widgets/tweet_button.html#url=http%3A%2F%2Fwebflow.com&amp;counturl=webflow.com&amp;text=Check%20out%20this%20site&amp;count=vertical&amp;size=m&amp;dnt=true" scrolling="no" frameborder="0" allowtransparency="true"
+            style="border: none; overflow: hidden; width: 55px; height: 62px;"></iframe>
+          </div>
+          <div class="w-widget w-widget-gplus social-widget">
+            <div class="g-plusone" data-href="http://webflow.com" data-size="tall" data-annotation="bubble" data-width="120" data-recommendations="false" id="___plusone_0" style="width: 50px; height: 60px; text-indent: 0px; margin: 0px; padding: 0px; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; background: transparent;"></div>
+          </div>
+          <div class="w-widget w-widget-facebook social-widget">
+            <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2Fwebflow&amp;layout=box_count&amp;locale=en_US&amp;action=like&amp;show_faces=false&amp;share=false" scrolling="no" frameborder="0" allowtransparency="true" style="border: none; overflow: hidden; width: 55px; height: 65px;"></iframe>
+          </div>
+        </div>
+        <div class="w-col w-col-6 center">
+          <img src="images/Pier Field.png" width="271" alt="5443fc800fdff0c53ed0417f_Pier%20Field.png">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section purple">
+    <div class="w-container">
+      <div class="text"><em>“Your go to Tournament Planning App”</em>
+      </div>
+      <div class="footer-text"></div>
+    </div>
+  </div>
+  <div class="section grey footer">
+    <div class="w-container">
+      <div class="button-block">
+        <a class="w-inline-block social-button" href="mailto:support@webflow.com?subject=Hello!" target="_blank">
+          <img src="images/email-icon.png" width="21px" alt="52af8da2aed63fb91400000f_email-icon.png">
+        </a>
+        <a class="w-inline-block social-button" href="http://facebook.com/webflowapp" target="_blank">
+          <img src="images/facebook-icon.png" width="21px" alt="52af8da8aed63fb914000010_facebook-icon.png">
+        </a>
+        <a class="w-inline-block social-button" href="http://twitter.com/webflowapp" target="_blank">
+          <img src="images/twitter-icon.png" width="21px" alt="52af8db1aed63fb914000011_twitter-icon.png">
+        </a>
+      </div>
+      <img class="logo-in-footer" src="images/TourneyPMPlogo.png" width="100" alt="5443b4680fdff0c53ed03f9e_TourneyPMP%20Logo.png">
+    </div>
+  </div>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script type="text/javascript" src="js/webflow.js"></script>
+  <!--[if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
+</body>
+</html>
